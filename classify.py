2c699342f9fe9e8c388ea330eb26c61af8248ec4
Paul <paul.asquin@gmail.com>
2018-12-01 23:29:54 +0100

    minor changes to PEP structure

--- classify.py
+++ classify.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def classify(trainSet, trainLabels, testSet):
-	
-	predictedLabels = np.zeros(testSet.shape[0])
-	
-	for i in range(testSet.shape[0]):
-		if testSet[i,2] == 'female':
-			predictedLabels[i] = 1
 
-	return predictedLabels
+def classify(trainSet, trainLabels, testSet):
+    predictedLabels = np.zeros(testSet.shape[0])
+
+    for i in range(testSet.shape[0]):
+        if testSet[i, 2] == 'female':
+            predictedLabels[i] = 1
+
+    return predictedLabels
