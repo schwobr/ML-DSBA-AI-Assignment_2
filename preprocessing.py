9296475b96c3f0fc9987731a412e6da4eda87173
Paul <paul.asquin@gmail.com>
2018-12-02 11:30:18 +0100

    refactor function names

--- preprocessing.py
+++ preprocessing.py
@@ -3,39 +3,43 @@ import pandas as pd
 from sklearn import preprocessing
 
 
-def changeGender(data):
+def change_gender(data):
     data.loc[data['Sex'] == 'male', 'Sex'] = 0
     data.loc[data['Sex'] == 'female', 'Sex'] = 1
 
-def ChangeEmbarked (data):
+
+def change_embarked(data):
     data.loc[data['Embarked'] == 'C', 'Embarked'] = 0
     data.loc[data['Embarked'] == 'Q', 'Embarked'] = 1
     data.loc[data['Embarked'] == 'S', 'Embarked'] = 2
     data.loc[data['Embarked'].isnull(), 'Embarked'] = data['Embarked'].value_counts().index[0]
 
-def ChangeName (data):
-    l=[s for l in data['Name'].str.split(' ') for s in l if ('.' in s and 'L.' not in s)]
-    data['Name']=l
-    n = data.index.values[0]
-    for i in range(n, data['Name'].size+n):
-        for j in range(len(l)):        
-            if(l[j]==data.at[i,'Name']):
-                data.at[i,'Name']=j
 
-def missingAges(data):
-    mean = round(data['Age'].mean(),1)
+def change_name(data):
+    l = [s for l in data['Name'].str.split(' ') for s in l if ('.' in s and 'L.' not in s)]
+    data['Name'] = l
+    n = data.index.values[0]
+    for i in range(n, data['Name'].size + n):
+        for j in range(len(l)):
+            if l[j] == data.at[i, 'Name']:
+                data.at[i, 'Name'] = j
+
+
+def missing_ages(data):
+    mean = round(data['Age'].mean(), 1)
     data.loc[data['Age'].isnull(), 'Age'] = mean
 
-def fareNaN(data):
+
+def fare_NaN(data):
     if data.isna().any()['Fare']:
-        data.loc[data['Fare'].isnull(), 'Fare'] = round(data['Fare'].mean(),1)
-    
+        data.loc[data['Fare'].isnull(), 'Fare'] = round(data['Fare'].mean(), 1)
+
+
 def preprocess(data):
-    data.drop(['Ticket','Cabin'], axis=1, inplace=True)
-    changeGender(data)
-    ChangeEmbarked(data)
-    ChangeName(data)
-    missingAges(data)
-    fareNaN(data)
-    print(data.head())
-    return (preprocessing.scale(data.values))
+    data.drop(['Ticket', 'Cabin'], axis=1, inplace=True)
+    change_gender(data)
+    change_embarked(data)
+    change_name(data)
+    missing_ages(data)
+    fare_NaN(data)
+    return preprocessing.scale(data.values)
